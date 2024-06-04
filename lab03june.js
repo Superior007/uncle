3ef79f92ccd7dc24edd6c3950a9fc1928bb5c7be
Superior007 <saifkhantheupperclass@gmail.com>
2024-06-04 21:37:23 +0500

    Add files via upload

---
+++ lab03june.js
@@ -0,0 +1,40 @@
+function twoTable(){
+    
+    var a ="";
+    
+    for(var i=1; i<=10; i++){
+       a=a+"<br>"+ 2+ " x " + i + " = " + 2*i+"<br>";
+       
+    }
+    document.getElementById("a").innerHTML = a;
+}
+function threeTable(){
+    
+    var a ="";
+    
+    for(var i=1; i<=10; i++){
+       a=a+"<br>"+ 3+ " x " + i + " = " + 3*i+"<br>";
+       
+    }
+    document.getElementById("b").innerHTML = a;
+}
+function fourTable(){
+    
+    var a ="";
+    
+    for(var i=1; i<=10; i++){
+       a=a+"<br>"+ 4+ " x " + i + " = " + 4*i+"<br>";
+       
+    }
+    document.getElementById("c").innerHTML = a;
+}
+function fiveTable(){
+    
+    var a ="";
+    
+    for(var i=1; i<=10; i++){
+       a=a+"<br>"+ 5+ " x " + i + " = " + 5*i+"<br>";
+       
+    }
+    document.getElementById("d").innerHTML = a;
+}
